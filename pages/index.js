be2ca6a65bdadbbcb3222edfa8347aaf0b8d05d8
codetheory <hello@timothy.is>
2017-09-22 03:08:09 +0200

    Show plugins page by default

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,3 @@
-import Header from '../components/Header'
-import Meta from '../components/Meta'
+import Plugins from './plugins'
 
-export default () => (
-  <div className="main">
-    <Meta />
-    <Header />
-    <p>hello world</p>
-  </div>
-)
+export default Plugins
