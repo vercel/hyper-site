b7875e1781389bbab4eb00a39857403513fcfc4e
Timothy <CodeTheory@users.noreply.github.com>
2017-10-09 15:18:37 +0100

    Revert "Correction of search component styles"

--- components/Search.js
+++ components/Search.js
@@ -5,11 +5,12 @@ export default () => (
         background: transparent;
         border: none;
         color: white;
-        font-size: 18px;
+        font-size: 14px;
         font-size: 1.4rem;
         font-weight: 400;
-        width: 6rem;
-        margin-top: 1px;
+        transform: translateX(50%);
+        line-height: 2.3rem;
+        left: 52%;
       }
     `}</style>
 
