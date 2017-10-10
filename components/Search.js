2a7da376aa75d8219d1fb1fca60b11a3a5156621
Mark <markdrew53@gmail.com>
2017-10-10 07:03:44 -0500

    style modifications in the input

--- components/Search.js
+++ components/Search.js
@@ -1,20 +1,48 @@
-export default () => (
- <div>
+import IconSearch from '../static/search.svg'
+import PropTypes from 'prop-types'
+
+const Search = () => (
+  <div>
     <style jsx>{`
-      input {
+      .search {
         background: transparent;
         border: none;
         color: white;
         font-size: 14px;
         font-size: 1.4rem;
         font-weight: 400;
-        transform: translateX(50%);
+        transform: translateX(40%);
         line-height: 2.3rem;
-        left: 52%;
+        left: 40%;
+        width: 70px;
+        padding-left: 5px;
+      }
+
+      .icon {
+        display: inline;
+        background: transparent;
+        border: none;
+        margin-left: 22px;
+        width: 0px;
+      }
+
+      .form {
+        display: inline;
       }
     `}</style>
-
-  <input type="text" placeholder="Search..." maxLength="30"/>
- </div>
+    <form className="form">
+      <input
+        className="search"
+        type="text" 
+        placeholder="Search..." 
+        maxLength="30"
+      />
+    </form>
+    
+    <button className="icon">
+     <IconSearch/>
+    </button>
+  </div>
 )
 
+export default Search
