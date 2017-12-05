a01255f88b289d61fc1687e434bb604b996ccb34
codetheory <hello@timothy.is>
2017-12-05 19:47:06 +0000

    Add filtering to plugins and themes pages

--- components/PluginsList.js
+++ components/PluginsList.js
@@ -1,22 +1,61 @@
+import React from 'react'
 import Plugin from './Plugin'
+import {
+  plugins as featuredPlugins,
+  themes as featuredThemes
+} from '../featured.json'
 
-export default ({ plugins }) => (
-  <div className="plugins-list">
-    {plugins.map((plugin, i) => (
-      <div key={plugin.package.name} className="plugin">
-        <Plugin {...plugin.package} />
+export default class extends React.Component {
+  constructor(props) {
+    super()
+
+    this.orderPlugins = this.orderPlugins.bind(this)
+  }
+
+  orderPlugins(plugins, orderBy) {
+    let sortedPlugins
+
+    if (orderBy === 'newest') {
+      const copy = [...plugins]
+
+      sortedPlugins = copy.sort((objA, objB) => {
+        if (objA.package.date < objB.package.date) return 1
+        if (objA.package.date > objB.package.date) return -1
+        return 0
+      })
+    } else if (orderBy === 'featured') {
+      sortedPlugins = plugins.filter(
+        plugin =>
+          featuredPlugins.includes(plugin.package.name) ||
+          featuredThemes.includes(plugin.package.name)
+      )
+    }
+
+    return sortedPlugins || plugins
+  }
+
+  render() {
+    const plugins = this.orderPlugins(this.props.plugins, this.props.filteredBy)
+
+    return (
+      <div className="plugins-list">
+        {plugins.map((plugin, i) => (
+          <div key={plugin.package.name} className="plugin">
+            <Plugin {...plugin.package} />
+          </div>
+        ))}
+
+        <style jsx>{`
+          .plugins-list {
+            padding-top: 48px;
+          }
+
+          .plugin:not(:last-of-type) {
+            border-bottom: 1px solid #333333;
+            margin-bottom: 16px;
+          }
+        `}</style>
       </div>
-    ))}
-
-    <style jsx>{`
-      .plugins-list {
-        padding-top: 48px;
-      }
-
-      .plugin:not(:last-of-type) {
-        border-bottom: 1px solid #333333;
-        margin-bottom: 16px;
-      }
-    `}</style>
-  </div>
-)
+    )
+  }
+}
--- pages/plugins.js
+++ pages/plugins.js
@@ -1,19 +1,40 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import PluginsList from '../components/PluginsList'
+import Filter from '../components/Filter'
 import getPlugins from '../lib/get-plugins'
 
 export default class extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      filter: 'featured'
+    }
+
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+  }
+
   static async getInitialProps() {
     const plugins = await getPlugins('plugins')
     return { plugins }
+  }
+
+  handleFilterChange(filter) {
+    this.setState({
+      filter
+    })
   }
 
   render() {
     const plugins = this.props.plugins.results
     return (
       <Layout>
-        <PluginsList plugins={plugins} />
+        <Filter
+          handleFilterChange={this.handleFilterChange}
+          currentFilter={this.state.filter}
+        />
+        <PluginsList plugins={plugins} filteredBy={this.state.filter} />
       </Layout>
     )
   }
