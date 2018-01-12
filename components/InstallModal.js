36f036edd0679772f6fedb853c76e968d6399f72
codetheory <hello@timothy.is>
2018-01-12 02:19:18 +0000

    Add install modal accessible by the PluginInfo component wherever it is

---
+++ components/InstallModal.js
@@ -0,0 +1,93 @@
+export default class extends React.Component {
+  componentDidUpdate() {
+    const body = window.document.body
+
+    if (this.props.isOpen === true) {
+      body.classList.add('has-modal-open')
+    } else {
+      body.classList.remove('has-modal-open')
+    }
+  }
+
+  render() {
+    if (this.props.isOpen === false) {
+      return null
+    }
+
+    return (
+      <div className="modal-container">
+        <div className="modal">
+          <h3>Install {this.props.name}</h3>
+          <p>
+            Use hyper-cli to install {this.props.name} by entering the following
+            into Hyper.app:
+          </p>
+          <pre>hyper install {this.props.name}</pre>
+        </div>
+        <div
+          className="modal-backdrop"
+          onClick={() => this.props.closeModal()}
+        />
+
+        <style jsx>{`
+          :global(body.has-modal-open) {
+            overflow: hidden;
+          }
+
+          .modal-container {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            z-index: 1000;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+
+          .modal-backdrop {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background: rgba(0, 0, 0, 0.25);
+          }
+
+          .modal {
+            background: white;
+            color: black;
+            padding: 40px;
+            position: relative;
+            z-index: 1200;
+            max-width: 100%;
+            width: 400px;
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+          }
+
+          .modal h3 {
+            margin-top: 0;
+            font-weight: 400;
+          }
+
+          .modal pre {
+            border: 1px solid #eaeaea;
+            color: #bd10e0;
+            padding: 12px 0;
+          }
+
+          .modal pre::before {
+            content: '$';
+            margin-right: 12px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
--- components/PluginInfo.js
+++ components/PluginInfo.js
@@ -1,73 +1,124 @@
 import Gravatar from 'react-gravatar'
+import InstallModal from './InstallModal'
 
-export default ({ plugin }) => (
-  <div className="plugin-info">
-    <div className="plugin-info__author border-followed">
-      <Gravatar
-        className="plugin-info__avatar"
-        email={plugin.metadata.publisher.email}
-      />
-      <span>{plugin.metadata.publisher.username}</span>
-    </div>
+export default class extends React.Component {
+  constructor() {
+    super()
 
-    <span className="border-followed">
-      {plugin.npm.downloads[2].count.toLocaleString()} downloads in the last
-      month
-    </span>
+    this.state = {
+      isModalOpen: false
+    }
 
-    <a
-      className="plugin-info__link"
-      href={plugin.metadata.links.repository}
-      target="_blank"
-    >
-      View source code
-    </a>
+    this.openInstallModal = this.openInstallModal.bind(this)
+    this.closeInstallModal = this.closeInstallModal.bind(this)
+  }
 
-    <div className="plugin-info__version border-followed">
-      Version {plugin.metadata.version}
-    </div>
-    <a href="#install">Installation</a>
+  openInstallModal() {
+    this.setState({
+      isModalOpen: true
+    })
 
-    <style jsx>{`
-      .plugin-info {
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        background: black;
-        height: 6.4rem;
-        display: flex;
-        align-items: center;
-        font-size: 1.2rem;
-        padding: 0 40px;
-      }
+    console.log(this.props)
+  }
 
-      .plugin-info__author {
-        display: flex;
-        align-items: center;
-      }
+  closeInstallModal() {
+    this.setState({
+      isModalOpen: false
+    })
+  }
 
-      :global(.plugin-info__avatar) {
-        border-radius: 50%;
-        width: 28px;
-        height: 28px;
-        margin-right: 8px;
-      }
+  render() {
+    const { plugin } = this.props
 
-      .plugin-info__version {
-        margin-left: auto;
-      }
+    return (
+      <div>
+        <InstallModal
+          name={plugin.metadata.name}
+          isOpen={this.state.isModalOpen}
+          closeModal={this.closeInstallModal}
+        />
 
-      .plugin-info__link {
-        text-transform: uppercase;
-        font-size: 1rem;
-      }
+        <div className="plugin-info">
+          <div className="plugin-info__author border-followed">
+            <Gravatar
+              className="plugin-info__avatar"
+              email={plugin.metadata.publisher.email}
+            />
+            <span>{plugin.metadata.publisher.username}</span>
+          </div>
 
-      .border-followed {
-        margin-right: 8px;
-        padding-right: 8px;
-        border-right: 1px solid #333333;
-      }
-    `}</style>
-  </div>
-)
+          <span className="border-followed">
+            {plugin.npm.downloads[2].count.toLocaleString()} downloads in the
+            last month
+          </span>
+
+          <a
+            className="plugin-info__link"
+            href={plugin.metadata.links.repository}
+            target="_blank"
+          >
+            View source code
+          </a>
+
+          <div className="plugin-info__version border-followed">
+            Version {plugin.metadata.version}
+          </div>
+
+          <a className="plugin-info__install" onClick={this.openInstallModal}>
+            Install
+          </a>
+
+          <style jsx>{`
+            .plugin-info {
+              position: fixed;
+              bottom: 0;
+              left: 0;
+              width: 100%;
+              background: black;
+              height: 6.4rem;
+              display: flex;
+              align-items: center;
+              font-size: 1.2rem;
+              padding: 0 40px;
+            }
+
+            .plugin-info__author {
+              display: flex;
+              align-items: center;
+            }
+
+            :global(.plugin-info__avatar) {
+              border-radius: 50%;
+              width: 28px;
+              height: 28px;
+              margin-right: 8px;
+            }
+
+            .plugin-info__version {
+              margin-left: auto;
+            }
+
+            .plugin-info__link {
+              text-transform: uppercase;
+              font-size: 1rem;
+            }
+
+            .plugin-info__install {
+              cursor: pointer;
+              border-radius: 2px;
+              background: #50e3c2;
+              color: black;
+              padding: 0 16px;
+            }
+
+            .border-followed {
+              margin-right: 8px;
+              padding-right: 8px;
+              border-right: 1px solid #333333;
+            }
+          `}</style>
+        </div>
+      </div>
+    )
+  }
+}
