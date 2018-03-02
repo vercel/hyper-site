8ffc73ce22fcfbdedd07d274ea30be7803f4ed6d
codetheory <hello@timothy.is>
2018-03-02 15:41:32 +0000

    Search error and page titles

--- components/Meta.js
+++ components/Meta.js
@@ -19,6 +19,7 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <title>Hyper Store</title>
     </Head>
     <style jsx global>{`
       /*
