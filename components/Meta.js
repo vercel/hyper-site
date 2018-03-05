e8f0e33231cf7e45a4c9a4d4f2c61dae62a7f8f5
codetheory <hello@timothy.is>
2018-03-05 16:49:47 +0000

    Add favicons

--- components/Meta.js
+++ components/Meta.js
@@ -20,6 +20,54 @@ export default () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <title>Hyper Store</title>
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="57x57"
+        href="/static/apple-touch-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="114x114"
+        href="/static/apple-touch-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="72x72"
+        href="/static/apple-touch-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="144x144"
+        href="/static/apple-touch-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="120x120"
+        href="/static/apple-touch-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="152x152"
+        href="/static/apple-touch-icon-152x152.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/static/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/static/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <meta name="application-name" content="Hyper Store" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta
+        name="msapplication-TileImage"
+        content="/static/mstile-144x144.png"
+      />
     </Head>
     <style jsx global>{`
       /*
