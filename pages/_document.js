8a0d96d2ad89f4dc2e9b757fd5d340a3c21d8ef6
timothyis <hello@timothy.is>
2018-04-16 15:09:05 +0100

    Add custom document and analytics

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-117491914-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-117491914-1');
+          `
+            }}
+          />
+        </body>
+      </html>
+    )
+  }
+}
