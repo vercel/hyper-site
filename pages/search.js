93bcb8b460f7119a9fb28026f7c8228d6a2cf880
Luis Alvarez <luisito453@gmail.com>
2019-08-22 20:56:34 -0500

    Removed the usage of props.url in /search

--- pages/search.js
+++ pages/search.js
@@ -1,38 +1,42 @@
 import React from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Layout from '../components/Layout'
 
-export default class extends React.Component {
-  render() {
-    return (
-      <div>
-        <Head>
-          {this.props.url.query.q ? (
-            <title>Hyper Store - Search for "{this.props.url.query.q}"</title>
-          ) : (
-            <title>Hyper Store - No search term</title>
-          )}
-        </Head>
-        <Layout query={this.props.url.query.q}>
-          {!this.props.url.query.q && (
-            <div className="search__error">
-              Please enter a search term to find a plugin or theme
-            </div>
-          )}
-          <style jsx>{`
-            .search__error {
-              width: 100%;
-              text-align: center;
-              height: calc(100vh - 122px);
-              min-height: 124px;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              font-size: 1.4rem;
-            }
-          `}</style>
-        </Layout>
-      </div>
-    )
-  }
+const Search = () => {
+  const router = useRouter()
+  const { q } = router.query
+
+  return (
+    <div>
+      <Head>
+        {q ? (
+          <title>Hyper Store - Search for "{q}"</title>
+        ) : (
+          <title>Hyper Store - No search term</title>
+        )}
+      </Head>
+      <Layout query={q}>
+        {!q && (
+          <div className="search__error">
+            Please enter a search term to find a plugin or theme
+          </div>
+        )}
+        <style jsx>{`
+          .search__error {
+            width: 100%;
+            text-align: center;
+            height: calc(100vh - 122px);
+            min-height: 124px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 1.4rem;
+          }
+        `}</style>
+      </Layout>
+    </div>
+  )
 }
+
+export default Search
