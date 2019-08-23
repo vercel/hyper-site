723f700bd67c7136edc02d6dcce4ca2590d62386
Luis Alvarez <luisito453@gmail.com>
2019-08-23 13:20:28 -0500

    Moved the mdx post to an actual page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const withMDX = require('@zeit/next-mdx')()
+const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
-  target: 'serverless'
+  target: 'serverless',
+  pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'mdx']
 })
