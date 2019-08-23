313129a1ff142b17180d725e38abe1cf03068b52
Luis Alvarez <luisito453@gmail.com>
2019-08-23 13:29:16 -0500

    Updated the themes page to match plugins

--- pages/themes.js
+++ pages/themes.js
@@ -1,92 +1,50 @@
 import React from 'react'
-import { withRouter } from 'next/router'
+import { withRouter, useRouter } from 'next/router'
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import PluginsList from '../components/PluginsList'
-import Filter from '../components/Filter.js'
+import Filter from '../components/Filter'
 import SubmitButton from '../components/SubmitButton'
 import getPlugins from '../lib/get-plugins'
 
-class Themes extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      filter: 'featured'
-    }
-
-    this.handleFilterChange = this.handleFilterChange.bind(this)
-  }
-
-  static async getInitialProps({ res }) {
-    const themes = await getPlugins({ type: 'theme' })
-    if (res) {
-      res.setHeader('Cache-Control', 's-maxage=7200, stale-while-revalidate')
-    }
-    return { themes }
-  }
-
-  componentWillMount() {
-    const requestedFilter = this.readFilterFromURL()
-    if (requestedFilter && this.state.filter !== requestedFilter) {
-      this.setState({
-        filter: requestedFilter
-      })
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.router.asPath !== this.props.router.asPath) {
-      const newFilter = this.readFilterFromURL()
-      this.handleFilterChange(newFilter)
-    }
-  }
-
-  handleFilterChange(newFilter) {
-    if (newFilter) {
-      this.updateFilterURL(newFilter)
-    }
-
-    this.setState({
-      filter: newFilter || 'featured'
-    })
-  }
-
-  readFilterFromURL() {
-    return this.props.router.asPath.split('?')[1]
-  }
-
-  updateFilterURL(newFilter) {
+const Themes = () => {
+  const router = useRouter()
+  const [plugins, setPlugins] = React.useState([])
+  const [filter, setFilter] = React.useState()
+  const updateFilter = newFilter => {
     const href = `/themes?${newFilter}`
-    this.props.router.push(href, href, { shallow: true })
+    router.push(href, href, { shallow: true })
   }
 
-  render() {
-    const themes = this.props.themes
-    return (
-      <Layout>
-        <Head>
-          <title>Hyper Store - Themes</title>
-        </Head>
-        <div className="plugins-heading container">
-          <Filter
-            handleFilterChange={this.handleFilterChange}
-            currentFilter={this.state.filter}
-          />
-          <SubmitButton href="https://github.com/zeit/hyper-site/wiki/Submitting-a-new-plugin-or-theme-to-Hyper-Store">
-            Submit a Theme
-          </SubmitButton>
-        </div>
-        <PluginsList plugins={themes} filteredBy={this.state.filter} />
+  React.useEffect(() => {
+    setPlugins(getPlugins({ type: 'theme' }))
+  }, [])
 
-        <style jsx>{`
-          .plugins-heading {
-            position: relative;
-          }
-        `}</style>
-      </Layout>
-    )
-  }
+  React.useEffect(() => {
+    const path = router.asPath.split('?')[1]
+    setFilter(path && path.replace('=', ''))
+  }, [router.asPath])
+
+  return (
+    <Layout>
+      <Head>
+        <title>Hyper Store - Plugins</title>
+      </Head>
+      <div className="plugins-heading container">
+        <Filter handleFilterChange={updateFilter} currentFilter={filter} />
+        <SubmitButton href="https://github.com/zeit/hyper-site/wiki/Submitting-a-new-plugin-or-theme-to-Hyper-Store">
+          Submit a Plugin
+        </SubmitButton>
+      </div>
+      <PluginsList plugins={plugins} filteredBy={filter} />
+
+      <style jsx>{`
+        .plugins-heading {
+          position: relative;
+        }
+      `}</style>
+    </Layout>
+  )
 }
 
-export default withRouter(Themes)
+export default Themes
