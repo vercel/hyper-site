967fde2117e75b2f4f4a67738a04bfc03f667dbd
Luis Alvarez <luisito453@gmail.com>
2019-08-22 20:49:00 -0500

    Moved the plugins page to use hooks

--- pages/plugins/index.js
+++ pages/plugins/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withRouter } from 'next/router'
+import { withRouter, useRouter } from 'next/router'
 import Head from 'next/head'
 import Layout from '../../components/Layout'
 import PluginsList from '../../components/PluginsList'
@@ -7,79 +7,38 @@ import Filter from '../../components/Filter'
 import SubmitButton from '../../components/SubmitButton'
 import getPlugins from '../../lib/get-plugins'
 
-class Plugins extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      filter: 'featured'
-    }
-    this.plugins = getPlugins({ type: 'plugin' })
-    this.handleFilterChange = this.handleFilterChange.bind(this)
-  }
-
-  componentWillMount() {
-    const requestedFilter = this.readFilterFromURL()
-    if (requestedFilter && this.state.filter !== requestedFilter) {
-      this.setState({
-        filter: requestedFilter
-      })
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.router.asPath !== this.props.router.asPath) {
-      const newFilter = this.readFilterFromURL()
-      this.handleFilterChange(newFilter)
-    }
-  }
-
-  handleFilterChange(newFilter) {
-    if (newFilter) {
-      this.updateFilterURL(newFilter)
-    }
-
-    this.setState({
-      filter: newFilter || 'featured'
-    })
-  }
-
-  readFilterFromURL() {
-    return this.props.router.asPath.split('?')[1]
-  }
-
-  updateFilterURL(newFilter) {
+const Plugins = () => {
+  const plugins = React.useMemo(() => getPlugins({ type: 'plugin' }), [])
+  const router = useRouter()
+  const filterFromUrl = router.asPath.split('?')[1] || 'featured'
+  const updateFilter = newFilter => {
     const href = `/plugins?${newFilter}`
-    this.props.router.push(href, href, { shallow: true })
+    router.push(href, href, { shallow: true })
   }
 
-  render() {
-    const { plugins } = this
+  return (
+    <Layout>
+      <Head>
+        <title>Hyper Store - Plugins</title>
+      </Head>
+      <div className="plugins-heading container">
+        <Filter
+          handleFilterChange={updateFilter}
+          currentFilter={filterFromUrl}
+        />
+        <SubmitButton href="https://github.com/zeit/hyper-site/wiki/Submitting-a-new-plugin-or-theme-to-Hyper-Store">
+          Submit a Plugin
+        </SubmitButton>
+      </div>
+      <PluginsList plugins={plugins} filteredBy={filterFromUrl} />
 
-    return (
-      <Layout>
-        <Head>
-          <title>Hyper Store - Plugins</title>
-        </Head>
-        <div className="plugins-heading container">
-          <Filter
-            handleFilterChange={this.handleFilterChange}
-            currentFilter={this.state.filter}
-          />
-          <SubmitButton href="https://github.com/zeit/hyper-site/wiki/Submitting-a-new-plugin-or-theme-to-Hyper-Store">
-            Submit a Plugin
-          </SubmitButton>
-        </div>
-        <PluginsList plugins={plugins} filteredBy={this.state.filter} />
-
-        <style jsx>{`
-          .plugins-heading {
-            position: relative;
-          }
-        `}</style>
-      </Layout>
-    )
-  }
+      <style jsx>{`
+        .plugins-heading {
+          position: relative;
+        }
+      `}</style>
+    </Layout>
+  )
 }
 
-export default withRouter(Plugins)
+export default Plugins
