6310aa8fd3822f95b8cb284025fbe413e25f5418
Luis Alvarez D <luis@zeit.co>
2019-09-03 11:17:40 -0500

    Only add used extensions
    Co-Authored-By: Lukáš Huvar <lukas@huvar.cz>

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
   target: 'serverless',
-  pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'mdx']
+  pageExtensions: ['js', 'mdx']
 })
