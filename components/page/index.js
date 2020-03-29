835190ad28b64745fb14d20e1006981ad0c2f2cf
vassbence <vbencze2@gmail.com>
2020-03-29 17:27:41 +0200

    added nprogress

--- components/page/index.js
+++ components/page/index.js
@@ -1,21 +1,11 @@
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Header from '../header'
 import SearchList from '../search-list'
-import { pageView as gTagPageView } from '../../lib/gtag'
 
 export default ({ children }) => {
-  const router = useRouter()
   const [search, setSearch] = useState('')
 
   const handleSearch = (newSearch) => setSearch(newSearch)
-
-  useEffect(() => {
-    router.events.on('routeChangeStart', (url) => gTagPageView(url))
-    return () => {
-      router.events.off('routeChangeStart', (url) => gTagPageView(url))
-    }
-  }, [])
 
   return (
     <>
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
 import '../styles/global.css'
 import { SWRConfig } from 'swr'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import { pageView as gTagPageView } from '../lib/gtag'
 
-export default ({ Component, pageProps }) => (
-  <SWRConfig value={{ fetcher: url => fetch(url).then(r => r.json()) }}>
-    <Component {...pageProps} />
-  </SWRConfig>
-)
+let timeout
+
+const start = (url) => {
+  timeout = setTimeout(NProgress.start, 200)
+  gTagPageView(url)
+}
+
+const done = () => {
+  clearTimeout(timeout)
+  NProgress.done()
+}
+
+Router.events.on('routeChangeStart', start)
+Router.events.on('routeChangeComplete', done)
+Router.events.on('routeChangeError', done)
+
+export default ({ Component, pageProps }) => {
+  return (
+    <SWRConfig value={{ fetcher: (url) => fetch(url).then((r) => r.json()) }}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
+}
