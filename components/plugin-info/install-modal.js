c2dd500eeca1964df971a4137cc6869920892786
vassbence <vbencze2@gmail.com>
2020-05-03 01:57:30 +0200

    better install modal

--- components/plugin-info/install-modal.js
+++ components/plugin-info/install-modal.js
@@ -1,14 +1,13 @@
-import { useEffect, useRef } from 'react'
-import selectText from '../../lib/select-text'
+import { useState, useEffect } from 'react'
+import { toClipboard } from 'copee'
 import styles from './install-modal.module.css'
 
 export default ({ pluginName, open, onClose }) => {
-  const preEl = useRef(null)
+  const [copied, setCopied] = useState(false)
 
   useEffect(() => {
     if (open) {
       document.body.style.overflow = 'hidden'
-      selectText(preEl.current)
     } else {
       document.body.style.overflow = 'auto'
     }
@@ -18,24 +17,35 @@ export default ({ pluginName, open, onClose }) => {
     return null
   }
 
+  const handleClick = () => {
+    const success = toClipboard(`hyper i ${pluginName}`)
+
+    if (success) {
+      setCopied(true)
+      setTimeout(() => setCopied(false), 1500)
+    }
+  }
+
   return (
     <div className={styles.root}>
       <div className={styles.backdrop} onClick={onClose} />
       <div className={styles.content}>
         <h3>Install {pluginName}</h3>
-        <p>
-          Use the <code>hyper</code> command, bundled with your Hyper app, to
-          install {pluginName} by entering the following into Hyper:
-        </p>
-        <pre ref={preEl}>hyper i {pluginName}</pre>
-        <a
-          href="https://github.com/zeit/hyper-plugins/wiki/Security-and-Hyper-plugins"
-          className={styles.security}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Security Notice
-        </a>
+        <p>Run the following command in Hyper:</p>
+        <pre>hyper i {pluginName}</pre>
+        <span className={styles.buttons}>
+          <a
+            href="https://github.com/zeit/hyper-plugins/wiki/Security-and-Hyper-plugins"
+            className={styles.security}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Security Notice
+          </a>
+          <button onClick={handleClick} disabled={copied}>
+            {copied ? 'Copied' : 'Copy'}
+          </button>
+        </span>
       </div>
     </div>
   )
