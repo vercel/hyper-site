d897465c383517a2c3e32bea1fb41f6c2c768cce
vassbence <vbencze2@gmail.com>
2020-05-13 19:12:02 +0200

    ssg store pages

--- pages/store/[name]/index.js
+++ pages/store/[name]/index.js
@@ -4,13 +4,16 @@ import PluginInfo from 'components/plugin-info'
 import plugins from 'plugins'
 import styles from 'styles/pages/store/index.module.css'
 
-export default ({ plugin }) => (
+export default ({ plugin, npmData }) => (
   <Page>
     <Head>
       <title>Hyper Store - {plugin.name}</title>
       <meta property="og:title" content={`Hyper Store - ${plugin.name}`} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={`https://hyper.is/store/${plugin.name}`} />
+      <meta
+        property="og:url"
+        content={`https://hyper.is/store/${plugin.name}`}
+      />
       <meta property="og:image" content={plugin.preview} />
       <meta property="og:description" content={plugin.description} />
       <meta property="og:site_name" content="Hyper Store" />
@@ -20,14 +23,19 @@ export default ({ plugin }) => (
       <h1 className={styles.name}>{plugin.name}</h1>
       <p>{plugin.description}</p>
       <img src={plugin.preview} alt={`${plugin.name} preview`} />
-      <PluginInfo variant="description" pluginName={plugin.name} />
+      <PluginInfo variant="description" npmData={npmData} />
     </div>
   </Page>
 )
 
-export const getStaticProps = ({ params }) => ({
-  props: { plugin: plugins.find((e) => e.name === params.name) },
-})
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(`https://api.npms.io/v2/package/${params.name}`)
+  const npmData = await res.json()
+
+  return {
+    props: { plugin: plugins.find((e) => e.name === params.name), npmData },
+  }
+}
 
 export const getStaticPaths = () => ({
   paths: plugins.map(({ name }) => ({ params: { name } })),
