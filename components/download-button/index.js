11469e324b1a836faf2702f25b86354cfe199ef9
Vass Bence <49574140+vassbence@users.noreply.github.com>
2020-05-19 20:08:24 +0200

    Patch (#181)
    * optimize all images, allow deployments on forks, cleanup download button
    
    * revert now config
    
    * update hero text
    
    * Bump lint-staged from 10.2.2 to 10.2.3 (#1)
    
    Bumps [lint-staged](https://github.com/okonet/lint-staged) from 10.2.2 to 10.2.3.
    - [Release notes](https://github.com/okonet/lint-staged/releases)
    - [Commits](https://github.com/okonet/lint-staged/compare/v10.2.2...v10.2.3)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Bump next from 9.4.0 to 9.4.1 (#2)
    
    Bumps [next](https://github.com/zeit/next.js) from 9.4.0 to 9.4.1.
    - [Release notes](https://github.com/zeit/next.js/releases)
    - [Commits](https://github.com/zeit/next.js/compare/v9.4.0...v9.4.1)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Paco <34928425+pacocoursey@users.noreply.github.com>

--- components/download-button/index.js
+++ components/download-button/index.js
@@ -1,76 +1,78 @@
 import { Apple, Linux, Windows, Download } from '../icons'
 import styles from './download-button.module.css'
+import useOs from 'lib/use-os'
 
-export default ({ os, fixedWidth }) => {
-  const cn = `${styles.root} ${fixedWidth ? styles.fixedWidth : ''}`
+const DownloadButton = () => {
+  const os = useOs()
+
   if (os === 'mac') {
     return (
-      <>
-        <a
-          className={cn}
-          href="https://releases.hyper.is/download/mac"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Apple size={16} />
-          <strong>Download Hyper for macOS</strong>
-        </a>
-      </>
+      <a
+        className={styles.root}
+        href="https://releases.hyper.is/download/mac"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Apple size={16} />
+        <strong>Download Hyper</strong>
+      </a>
     )
   } else if (os === 'windows') {
     return (
       <a
-        className={cn}
+        className={styles.root}
         href="https://releases.hyper.is/download/win"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Windows size={16} />
-        <strong>Download Hyper for Windows</strong>
+        <strong>Download Hyper</strong>
       </a>
     )
   } else if (os === 'fedora') {
     return (
       <a
-        className={cn}
+        className={styles.root}
         href="https://releases.hyper.is/download/rpm"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Linux size={16} />
-        <strong>Download Hyper for Fedora</strong>
+        <strong>Download Hyper</strong>
       </a>
     )
   } else if (os === 'ubuntu') {
     return (
       <a
-        className={cn}
+        className={styles.root}
         href="https://releases.hyper.is/download/deb"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Linux size={16} />
-        <strong>Download Hyper for Ubuntu</strong>
+        <strong>Download Hyper</strong>
       </a>
     )
   } else if (os === 'linux') {
     return (
       <a
-        className={cn}
+        className={styles.root}
         href="https://releases.hyper.is/download/AppImage"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Linux />
-        <strong>Download Hyper for Linux</strong>
+        <strong>Download Hyper</strong>
       </a>
     )
   } else {
     return (
-      <a href="/#installation" className={cn}>
+      <a href="/#installation" className={styles.root}>
         <Download height={12} width={16} />
         <strong>Download Hyper</strong>
       </a>
     )
   }
 }
+
+export default DownloadButton
