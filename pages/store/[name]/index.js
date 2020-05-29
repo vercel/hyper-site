788a6c5e3a43f54ca30beb164370f4397c0d05a0
vassbence <vbencze2@gmail.com>
2020-05-29 18:30:39 +0200

    Update plugin submission guidelines, serve preview images

--- pages/store/[name]/index.js
+++ pages/store/[name]/index.js
@@ -3,6 +3,8 @@ import Page from 'components/page'
 import PluginInfo from 'components/plugin-info'
 import plugins from 'plugins'
 import styles from 'styles/pages/store/index.module.css'
+import fs from 'fs'
+import { join } from 'path'
 
 export default ({ plugin, npmData }) => (
   <Page
@@ -15,24 +17,38 @@ export default ({ plugin, npmData }) => (
         property="og:url"
         content={`https://hyper.is/store/${plugin.name}`}
       />
-      <meta property="og:image" content={plugin.preview} />
+      <meta property="og:image" content={`https://hyper.is${plugin.preview}`} />
     </Head>
 
     <div className={styles.root}>
       <h1 className={styles.name}>{plugin.name}</h1>
       <p>{plugin.description}</p>
-      <img src={plugin.preview} alt={`${plugin.name} preview`} />
+      <img src={plugin.preview} alt={`${plugin.name}'s preview image`} />
       <PluginInfo variant="description" npmData={npmData} />
     </div>
   </Page>
 )
 
 export const getStaticProps = async ({ params }) => {
-  const res = await fetch(`https://api.npms.io/v2/package/${params.name}`)
-  const npmData = await res.json()
+  const npmData = await (
+    await fetch(`https://api.npms.io/v2/package/${params.name}`)
+  ).json()
+
+  // get preview image name, e.g.: 'hyperpower.gif'
+  const previewImageSrc = fs
+    .readdirSync(join(process.cwd(), 'public', 'store'))
+    .find((f) => f.includes(params.name))
+
+  // full relative path to the image, this now can be used as an img's src
+  const preview = `/store/${previewImageSrc}`
+
+  const plugin = { ...plugins.find((e) => e.name === params.name), preview }
 
   return {
-    props: { plugin: plugins.find((e) => e.name === params.name), npmData },
+    props: {
+      plugin,
+      npmData,
+    },
     unstable_revalidate: 60 * 60 * 24,
   }
 }
