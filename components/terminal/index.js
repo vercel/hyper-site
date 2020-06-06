10eada3ed6037948912bfbc13abf3e26595e639d
vassbence <vbencze2@gmail.com>
2020-06-06 21:06:50 +0200

    better animation

--- components/terminal/index.js
+++ components/terminal/index.js
@@ -18,54 +18,84 @@ const Terminal = () => (
         <span className={styles.icon} />
       </div>
       <Keyframes component="div" className={styles.body}>
-        <Frame duration={1000}>
+        <Frame duration={2000}>
           <Line />
         </Frame>
-        <Frame duration={150}>
+        <Frame duration={100}>
           <Line text="l" />
         </Frame>
-        <Frame duration={800}>
+        <Frame duration={500}>
           <Line text="ls" />
-        </Frame>
-        <Frame duration={1000}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line />
-        </Frame>
-        <Frame duration={150}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="c" />
-        </Frame>
-        <Frame duration={150}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="ca" />
-        </Frame>
-        <Frame duration={150}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat" />
-        </Frame>
-        <Frame duration={150}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat " />
-        </Frame>
-        <Frame duration={150}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat i" />
         </Frame>
         <Frame duration={600}>
           <Line text="ls" noCaret />
           <Line text="info.txt" noCaret noPrompt />
+          <Line />
+        </Frame>
+        <Frame duration={100}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="c" />
+        </Frame>
+        <Frame duration={70}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="ca" />
+        </Frame>
+        <Frame duration={70}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat" />
+        </Frame>
+        <Frame duration={60}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat " />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat i" />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
           <Line text="cat in" />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat inf" />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat info" />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat info." />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat info.t" />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat info.tx" />
+        </Frame>
+        <Frame duration={80}>
+          <Line text="ls" noCaret />
+          <Line text="info.txt" noCaret noPrompt />
+          <Line text="cat info.txt" />
         </Frame>
         <Frame duration={800}>
           <Line text="ls" noCaret />
           <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info.txt " />
+          <Line text="cat info.txt" />
         </Frame>
         <Frame>
           <Line text="ls" noCaret />
@@ -73,7 +103,7 @@ const Terminal = () => (
           <Line text="cat info.txt " noCaret />
           <Line
             text={
-              '- Hyper is an Electron-based terminal\n- Built on HTML/CSS/JS\n- Fully extensible!'
+              '- Hyper is an Electron-based terminal\n- Built on HTML/CSS/JS\n- Fully extensible'
             }
             noCaret
             noPrompt
