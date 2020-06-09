14594492c808b6f59f918aa120d2dda73eab989b
vassbence <vbencze2@gmail.com>
2020-06-09 05:16:53 +0200

    cleanup

--- components/terminal/index.js
+++ components/terminal/index.js
@@ -1,118 +1,93 @@
-import { Keyframes, Frame } from 'react-keyframes'
+import { Keyframes, Frame } from './react-keyframes.ts'
 import styles from './terminal.module.css'
+import { useState } from 'react'
+
+const sleepDuration = 500
+const getTypingDuration = () => 80 + 80 * (Math.random() - 0.5)
 
 const Line = ({ text, noPrompt = false, noCaret = false }) => (
-  <p>
+  <>
     {!noPrompt && <span>▲ ~ </span>}
     {text}
     {!noCaret && <span className={styles.caret} />}
-  </p>
+  </>
 )
 
-const Terminal = () => (
-  <div className={styles.root}>
-    <div className={styles.inner}>
-      <div className={styles.header}>
-        <span className={styles.icon} />
-        <span className={styles.icon} />
-        <span className={styles.icon} />
-      </div>
-      <Keyframes component="div" className={styles.body}>
-        <Frame duration={2000}>
-          <Line />
+const Terminal = () => {
+  const [lineCount, setLineCount] = useState(0)
+
+  const renderLine = (text) => {
+    const frames = []
+
+    // starting frame
+    frames.push(
+      <Frame duration={sleepDuration}>
+        <Line />
+      </Frame>
+    )
+
+    // typing out the line
+    for (let i = 0; i < text.length; i++) {
+      const isLastLetter = i === text.length - 1
+      const duration = isLastLetter ? sleepDuration : getTypingDuration()
+      frames.push(
+        <Frame duration={duration}>
+          <Line text={text.slice(0, i + 1)} />
         </Frame>
-        <Frame duration={100}>
-          <Line text="l" />
-        </Frame>
-        <Frame duration={500}>
-          <Line text="ls" />
-        </Frame>
-        <Frame duration={600}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line />
-        </Frame>
-        <Frame duration={100}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="c" />
-        </Frame>
-        <Frame duration={70}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="ca" />
-        </Frame>
-        <Frame duration={70}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat" />
-        </Frame>
-        <Frame duration={60}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat " />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat i" />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat in" />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat inf" />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info" />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info." />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info.t" />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info.tx" />
-        </Frame>
-        <Frame duration={80}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info.txt" />
-        </Frame>
-        <Frame duration={800}>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info.txt" />
-        </Frame>
-        <Frame>
-          <Line text="ls" noCaret />
-          <Line text="info.txt" noCaret noPrompt />
-          <Line text="cat info.txt " noCaret />
-          <Line
-            text={
-              '- Hyper is an Electron-based terminal\n- Built on HTML/CSS/JS\n- Fully extensible'
-            }
-            noCaret
-            noPrompt
-          />
-          <Line />
-        </Frame>
+      )
+    }
+
+    // ending frame
+    frames.push(
+      <Frame>
+        <Line text={text} noCaret />
+      </Frame>
+    )
+
+    return (
+      <Keyframes component="p" onEnd={() => setLineCount((c) => c + 1)}>
+        {frames}
       </Keyframes>
+    )
+  }
+
+  return (
+    <div className={styles.root}>
+      <div
+        className={`${styles.inner}${
+          lineCount >= 5 ? ' ' + styles.yellow : ''
+        }`}
+      >
+        <div className={styles.header}>
+          <span className={styles.icon} />
+          <span className={styles.icon} />
+          <span className={styles.icon} />
+        </div>
+        <div className={styles.body}>
+          {renderLine('# Hyper is an Electron-based terminal')}
+          {lineCount >= 1 && renderLine('# Built on HTML/CSS/JS')}
+          {lineCount >= 2 && renderLine('# Fully extensible')}
+          {lineCount >= 3 &&
+            renderLine("# For example, let's install a fresh theme")}
+          {lineCount >= 4 && renderLine('hyper i hyperyellow')}
+          {lineCount >= 5 && (
+            <>
+              <p className={styles.green}>
+                <Line
+                  text="hyperyellow installed successfully!"
+                  noPrompt
+                  noCaret
+                />
+              </p>
+              <p>
+                <Line />
+              </p>
+            </>
+          )}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Terminal
