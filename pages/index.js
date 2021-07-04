c243c2c2d2fa0d97441c7926e0ff76f673e18442
Hai Nguyen <github@ng-hai.dev>
2021-07-05 04:33:12 +0700

    Change ZEIT to Vercel (#240)
    Co-authored-by: ng-hai <git@ng-hai.dev>

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ const installationTableData = [
 
 export async function getStaticProps() {
   const res = await fetch(
-    'https://api.github.com/repos/zeit/hyper/releases/latest'
+    'https://api.github.com/repos/vercel/hyper/releases/latest'
   )
   const latestRelease = await res.json()
 
@@ -252,17 +252,17 @@ export default function HomePage({ latestRelease }) {
             <tbody>
               <tr>
                 <td>
-                  <a href="https://github.com/zeit/hyper/blob/master/app/keymaps/win32.json">
+                  <a href="https://github.com/vercel/hyper/blob/master/app/keymaps/win32.json">
                     Windows
                   </a>
                 </td>
                 <td>
-                  <a href="https://github.com/zeit/hyper/blob/master/app/keymaps/linux.json">
+                  <a href="https://github.com/vercel/hyper/blob/master/app/keymaps/linux.json">
                     Linux
                   </a>
                 </td>
                 <td>
-                  <a href="https://github.com/zeit/hyper/blob/master/app/keymaps/darwin.json">
+                  <a href="https://github.com/vercel/hyper/blob/master/app/keymaps/darwin.json">
                     macOS
                   </a>
                 </td>
@@ -463,7 +463,7 @@ export default function HomePage({ latestRelease }) {
                 <td>
                   A list of overrides for the color palette. The names of the
                   keys represent the "ANSI 16", which can all be seen{' '}
-                  <a href="https://github.com/zeit/hyper/blob/master/app/utils/colors.js">
+                  <a href="https://github.com/vercel/hyper/blob/master/app/utils/colors.js">
                     in the default config
                   </a>
                   .
@@ -609,7 +609,7 @@ export default function HomePage({ latestRelease }) {
         </p>
         <p>
           You can find additional details about plugin development{' '}
-          <a href="https://github.com/zeit/hyper/blob/master/PLUGINS.md">
+          <a href="https://github.com/vercel/hyper/blob/master/PLUGINS.md">
             in the Hyper repository
           </a>
           .
@@ -1024,7 +1024,7 @@ export default function HomePage({ latestRelease }) {
                   <p>
                     A custom mapper for the state properties that{' '}
                     <a
-                      href="https://github.com/zeit/hyper/tree/master/lib/containers"
+                      href="https://github.com/vercel/hyper/tree/master/lib/containers"
                       target="_blank"
                       rel="noopener"
                     >
@@ -1442,7 +1442,7 @@ export default function HomePage({ latestRelease }) {
         <p>
           All the{' '}
           <a
-            href="https://github.com/zeit/hyper/tree/master/lib/actions"
+            href="https://github.com/vercel/hyper/tree/master/lib/actions"
             target="_blank"
             rel="noopener"
           >
@@ -1647,7 +1647,7 @@ export default function HomePage({ latestRelease }) {
           <a
             target="_blank"
             rel="noopener"
-            href="https://github.com/zeit/hyperyellow/blob/29c4ac9748be74d7ad587b7077758ef26f6ce5c2/index.js#L1"
+            href="https://github.com/vercel/hyperyellow/blob/29c4ac9748be74d7ad587b7077758ef26f6ce5c2/index.js#L1"
           >
             code
           </a>
@@ -1746,7 +1746,7 @@ export default function HomePage({ latestRelease }) {
           <a
             target="_blank"
             rel="noopener"
-            href="https://github.com/zeit/hyperpower/blob/master/index.js"
+            href="https://github.com/vercel/hyperpower/blob/master/index.js"
           >
             its code
           </a>
@@ -1757,7 +1757,7 @@ export default function HomePage({ latestRelease }) {
           <a
             target="_blank"
             rel="noopener"
-            href="https://github.com/zeit/hyper/tree/master/lib/actions"
+            href="https://github.com/vercel/hyper/tree/master/lib/actions"
           >
             in the repository
           </a>
@@ -1847,7 +1847,7 @@ export default function HomePage({ latestRelease }) {
         <p>
           The extension then{' '}
           <a
-            href="https://github.com/zeit/hyperpower/blob/master/index.js#L51"
+            href="https://github.com/vercel/hyperpower/blob/master/index.js#L51"
             target="_blank"
             rel="noopener"
           >
